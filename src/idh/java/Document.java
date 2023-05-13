e597beea4062702c76a893263fa506d06bcc9ba7
skr4ll <skrall_pa@protonmail.com>
2023-05-13 13:58:29 +0200

    Aufgabe 1 - ArrayList, Funktion und for-Schleife hinzugefügt.

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,10 +3,31 @@ package idh.java;	//Start
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
-public class Document {
+public class Document implements Iterable<String> {
+	
+	class StringIterator implements Iterator<String>{
+		int currentPosition = -1;
+		
+		@Override
+		public boolean hasNext() {
+			return currentPosition < tokens.size()	-1;
+		}
+
+		@Override
+		public String next() {
+			currentPosition++;
+			return tokens.get(currentPosition);
+		}
+		
+	}
+	
 	String documentText;
-
+	ArrayList<String> tokens = new ArrayList<String>();		//Aus documentText werden hier die tokens reingeschrieben
+	
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
 		int ch;
@@ -17,8 +38,15 @@ public class Document {
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
 		return doc;
+	}
+
+	//Zur Tokenisierung von documentText
+	public void  tokenize() {
+		StringTokenizer st = new StringTokenizer(this.documentText);
+		while (st.hasMoreTokens()) {
+	         this.tokens.add(st.nextToken());
+	     }
 	}
 	
 	public String getDocumentText() {
@@ -31,6 +59,20 @@ public class Document {
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		//documentText tokenisieren lassen
+		d.tokenize();
+		//Über die ArrayList<String> im aktuellen Document Objekt (d) iterieren
+		for (String string : d) {
+			System.out.println(string);
+		}
+		
+	}	
+	
+
+	@Override
+	public Iterator<String> iterator() {
+		
+	return new StringIterator();
 	}
 	
 }
