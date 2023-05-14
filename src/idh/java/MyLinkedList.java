cbbebc960baeb0b1fc31ec65ff6b2da73f807eab
floeliasrux <frux@smail.uni-koeln.de>
2023-05-14 02:11:00 +0200

    Aufgabe 3 fertig

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -5,63 +5,79 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-
 public class MyLinkedList<T> implements List<T> {
 
 	/**
-	 * We only need to store the very first element of our list, 
-	 * because it will now whether there is a next element.
+	 * We only need to store the very first element of our list, because it will now
+	 * whether there is a next element.
 	 */
 	ListElement first;
-	
-	
+
 	@Override
 	public int size() {
-		// TODO Implement!
-		return 0;
+		int counter = 0;
+		ListElement current = this.first;
+		while (current != null) {
+			counter++;
+			current = current.next;
+		}
+		return counter;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return first == null;
+		return this.first == null;
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		// TODO Implement!
+		ListElement current = this.first;
+		while (current != null) {
+			if (current.value.equals(o)) {
+				return true;
+			}
+			current = current.next;
+		}
 		return false;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
 		return new Iterator<T>() {
-			ListElement next = first;
-			
+			ListElement next = MyLinkedList.this.first;
+
 			@Override
 			public boolean hasNext() {
-				return next != null;
+				return this.next != null;
 			}
 
 			@Override
 			public T next() {
-				T ret = next.value;
-				next = next.next;
+				T ret = this.next.value;
+				this.next = this.next.next;
 				return ret;
 			}
-			
+
 		};
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Implement!
-		return null;
+		int size = this.size();
+		Object[] array = new Object[size];
+		ListIterator<T> listIter = this.listIterator();
+		int i = 0;
+		while (listIter.hasNext()) {
+			array[i] = listIter.next();
+			i++;
+		}
+		return array;
 	}
 
 	@Override
 	public <E> E[] toArray(E[] a) {
-		if (a.length < size()) {
-			a = (E[]) new Object[size()];
+		if (a.length < this.size()) {
+			a = (E[]) new Object[this.size()];
 		}
 		int i = 0;
 		for (T t : this) {
@@ -73,45 +89,66 @@ public class MyLinkedList<T> implements List<T> {
 	@Override
 	public boolean add(T e) {
 		ListElement newListElement = new ListElement(e);
-		if (first == null)
-			first = newListElement;
-		else
-			last().next = newListElement;
+		if (this.first == null) {
+			this.first = newListElement;
+		} else {
+			this.last().next = newListElement;
+		}
 		return true;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		// TODO: Implement
+		ListElement current = this.first;
+		ListElement prev = null;
+		while (current != null) {
+			if (current.value.equals(o)) {
+				if (prev == null) {
+					this.first = current.next;
+				} else {
+					prev.next = current.next;
+				}
+				return true;
+			}
+			prev = current;
+			current = current.next;
+		}
 		return false;
+
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> c) {
-		for (Object o : c)
-			if (! contains(o))
+		for (Object o : c) {
+			if (!this.contains(o)) {
 				return false;
+			}
+		}
 		return true;
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends T> c) {
-		for (T t : c) 
+		for (T t : c) {
 			this.add(t);
+		}
 		return true;
 	}
 
 	@Override
 	public boolean addAll(int index, Collection<? extends T> c) {
-		// TODO Implement!
-		return false;
+		for (T t : c) {
+			this.add(t);
+		}
+		return true;
 	}
 
 	@Override
 	public boolean removeAll(Collection<?> c) {
 		boolean r = true;
-		for (Object o : c) 
+		for (Object o : c) {
 			r = r || this.remove(o);
+		}
 		return r;
 	}
 
@@ -122,41 +159,77 @@ public class MyLinkedList<T> implements List<T> {
 
 	@Override
 	public void clear() {
-		first = null;
+		this.first = null;
 	}
 
 	@Override
 	public T get(int index) {
-		return getElement(index).value;
+		return this.getElement(index).value;
 	}
 
 	@Override
 	public T set(int index, T element) {
-		// TODO: Implement
-		return null;
+		ListElement e = this.getElement(index);
+		T object = e.value;
+		e.value = element;
+		return object;
 	}
 
 	@Override
 	public void add(int index, T element) {
-		// TODO: Implement
+		ListElement newElement = new ListElement(element);
+		if (index == 0) {
+			newElement.next = this.first;
+			this.first = newElement;
+		} else {
+			ListElement prev = this.getElement(index - 1);
+			newElement.next = prev.next;
+			prev.next = newElement;
+		}
 	}
 
 	@Override
 	public T remove(int index) {
-		// TODO: Implement
-		return null;
+		ListElement prev = this.getElement(index - 1);
+		ListElement current = this.getElement(index);
+		ListElement nextElement = this.getElement(index + 1);
+
+		if (prev == null) {
+			this.first = nextElement;
+		} else {
+			prev.next = nextElement;
+		}
+		return current.value;
 	}
 
 	@Override
 	public int indexOf(Object o) {
-		// TODO: Implement
-		return 0;
+		ListElement current = this.first;
+		int index = 0;
+
+		while (current != null) {
+			if (current.value.equals(o)) {
+				return index;
+			}
+			current = current.next;
+			index++;
+		}
+		return -1;
 	}
 
 	@Override
 	public int lastIndexOf(Object o) {
-		// TODO: Implement
-		return 0;
+		ListElement current = this.first;
+		int lastIndex = -1;
+		int index = 0;
+		while (current != null) {
+			if (current.value.equals(o)) {
+				lastIndex = index;
+			}
+			current = current.next;
+			index++;
+		}
+		return lastIndex;
 	}
 
 	@Override
@@ -164,20 +237,20 @@ public class MyLinkedList<T> implements List<T> {
 		return new ListIterator<T>() {
 
 			ListElement previous = null;
-			ListElement next = first;
+			ListElement next = MyLinkedList.this.first;
 			int index;
-			
+
 			@Override
 			public boolean hasNext() {
-				return next != null;
+				return this.next != null;
 			}
 
 			@Override
 			public T next() {
-				previous = next;
-				T ret = next.value;
-				next = next.next;
-				index++;
+				this.previous = this.next;
+				T ret = this.next.value;
+				this.next = this.next.next;
+				this.index++;
 				return ret;
 			}
 
@@ -193,29 +266,29 @@ public class MyLinkedList<T> implements List<T> {
 
 			@Override
 			public int nextIndex() {
-				return index+1;
+				return this.index + 1;
 			}
 
 			@Override
 			public int previousIndex() {
-				return index-1;
+				return this.index - 1;
 			}
 
 			@Override
 			public void remove() {
-				previous.next = next.next;
+				this.previous.next = this.next.next;
 			}
 
 			@Override
 			public void set(T e) {
-				next.value = e;
+				this.next.value = e;
 			}
 
 			@Override
 			public void add(T e) {
-				throw new UnsupportedOperationException();				
+				throw new UnsupportedOperationException();
 			}
-			
+
 		};
 	}
 
@@ -228,43 +301,50 @@ public class MyLinkedList<T> implements List<T> {
 	public List<T> subList(int fromIndex, int toIndex) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	private class ListElement {
 		T value;
 		ListElement next;
-		
+
 		ListElement(T value) {
 			this.value = value;
 		}
 	}
-	
+
 	/**
-	 * Internal method that iterates over the list, returning the last element (i.e., the one whose next field is null)
+	 * Internal method that iterates over the list, returning the last element
+	 * (i.e., the one whose next field is null)
+	 *
 	 * @return
 	 */
 	private ListElement last() {
-		if (first == null)
+		if (this.first == null) {
 			return null;
-		ListElement current = first;
-		
-		while(current.next != null) {
+		}
+		ListElement current = this.first;
+
+		while (current.next != null) {
 			current = current.next;
 		}
 		return current;
 	}
-	
-	/** 
-	 * Internal method to get the list element (not the value) of the list at the specified index position.
+
+	/**
+	 * Internal method to get the list element (not the value) of the list at the
+	 * specified index position.
+	 *
 	 * @param index
 	 * @return
 	 */
 	private ListElement getElement(int index) {
-		if (isEmpty()) 
+		if (this.isEmpty()) {
 			return null;
-		ListElement current = first;
-		while(current != null) {
-			if (index == 0) 
+		}
+		ListElement current = this.first;
+		while (current != null) {
+			if (index == 0) {
 				return current;
+			}
 			index--;
 			current = current.next;
 		}
