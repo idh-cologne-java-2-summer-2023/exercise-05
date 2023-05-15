d0806d2ae1f6b9c42fb46b64da561fbba035c5b4
NilsKEA <nallstae@smail.uni-koeln.de>
2023-05-15 20:17:41 +0200

    Document done

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -1,36 +1,61 @@
 package idh.java;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
-public class Document {
-	String documentText;
+public class Document implements Iterable<String> {
+    private String documentText;
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    private Document(String documentText) {
+        this.documentText = documentText;
+    }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-	}
-	
+    public static Document loadFromFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return new Document(sb.toString());
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new TokenIterator();
+    }
+
+    private class TokenIterator implements Iterator<String> {
+        private StringTokenizer tokenizer;
+
+        public TokenIterator() {
+            tokenizer = new StringTokenizer(documentText);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return tokenizer.hasMoreTokens();
+        }
+
+        @Override
+        public String next() {
+            return tokenizer.nextToken();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Document document = Document.loadFromFile("data/dracula.txt");
+        for (String token : document) {
+            System.out.println(token);
+        }
+    }
 }
