c5186d7f0b971f9f9bb57613f946d3006175979d
RegalName <gabriel.pospischil@gmail.com>
2023-05-16 21:17:09 +0200

    added StringIterator class

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,9 +3,13 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
-public class Document {
+public class Document implements Iterable<String>{
 	String documentText;
+	
+	String[] documentTextTokens;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -31,6 +35,16 @@ public class Document {
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		
+	}
+
+	@Override
+	public Iterator<String> iterator() {
+//		StringTokenizer st = new StringTokenizer(documentText);
+//		while (st.hasMoreTokens()) {
+//			st.nextToken();
+//	     }
+		return new StringIterator();
 	}
 	
 }
---
+++ src/idh/java/StringIterator.java
@@ -0,0 +1,27 @@
+package idh.java;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class StringIterator implements Iterator<String> {
+
+	String documentText;
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+//	StringTokenizer st = new StringTokenizer(documentText);
+//	while (st.hasMoreTokens()) {
+//		st.nextToken();
+//	 }
