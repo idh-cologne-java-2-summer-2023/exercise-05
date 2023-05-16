9f935b088838ff12387f95627275e84cb4704c6c
joyandrico <jbachma9@smail.uni-koeln.de>
2023-05-16 02:01:09 +0200

    Aufgabe 1

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,24 +3,26 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
-public class Document {
+public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
+		while ((ch = fileReader.read()) != -1) {
 			b.append((char) ch);
 		}
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -29,8 +31,43 @@ public class Document {
 		this.documentText = documentText;
 	}
 	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
+	@Override
+	public Iterator<String> iterator() {
+		return new StringIterator();
 	}
-	
+
+	public final static void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		
+		Iterator<String> iter = d.iterator();
+		
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+		
+	}
+
+
+	class StringIterator implements Iterator<String> {
+
+		StringTokenizer tokenizer;
+
+
+		public StringIterator() {
+			this.tokenizer = new StringTokenizer(documentText);
+		}
+		
+
+		@Override
+		public boolean hasNext() {
+			return tokenizer.hasMoreTokens();
+		}
+
+		@Override
+		public String next() {
+			return tokenizer.nextToken();
+		}
+
+	}
+
 }
