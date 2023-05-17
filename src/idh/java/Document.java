e22faa403ecb6efe57a917f4a23f04362ec1f2d2
levineder <levineder@neuromancer.fritz.box>
2023-05-17 02:23:48 +0200

    done

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -7,7 +7,9 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
+	int currentpos = 0;
 	String documentText;
+	StringTokenizer st;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -19,10 +21,10 @@ public class Document implements Iterable<String> {
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-
+		
 		return doc;
 	}
-
+	
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -30,28 +32,49 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
+	
+	public static final void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		String documentText = d.getDocumentText();
+		Iterator<String> it  = d.iterator();
+		
+		//Versuch von Aufgabe 2
 
-	@Override
+		
+		
+		String [] words = documentText.split("\\s+");
+		SkipIterator<String> si = new SkipIterator<String>(3, it, words.length);
+		while(si.hasNext()) {
+		documentText = si.next();
+		System.out.println(documentText);
+	}
+
+		
+		//Aufgabe 1
+//		while(it.hasNext()) {
+//			documentText = it.next();
+//			System.out.println(documentText);
+//		}
+//		
+//		System.out.println();
+//		System.out.println("Ende");
+	}
+
 	public Iterator<String> iterator() {
+            
 		StringTokenizer st = new StringTokenizer(documentText);
 		return new Iterator<String>() {
+
 			@Override
 			public boolean hasNext() {
-				return st.hasMoreTokens();
+            return st.hasMoreTokens();
 			}
 
 			@Override
 			public String next() {
 				return st.nextToken();
 			}
-		};
+	};
+		
 	}
-
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		for (String token : d) {
-			System.out.println(token);
-		}
-	}
-
 }
---
+++ src/idh/java/SkipIterator.java
@@ -0,0 +1,33 @@
+package idh.java;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class SkipIterator<T> implements Iterator<T> {
+	int n;
+	Iterator<T> it;
+	int currentpos;
+	T e;
+	int length;
+	
+	public SkipIterator( int n, Iterator it, int length) {
+		this.n = n;
+		this.it = it;
+        this.length = length;
+	}
+
+	@Override
+	public boolean hasNext() {
+        return (currentpos + n) < length;
+	}
+	@Override
+	public T next() {
+		currentpos = currentpos + n;
+		for(int i = currentpos; i > 0; i--) {
+			e = it.next();
+		}
+		
+
+		return e;
+}
+}
